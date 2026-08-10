efda8702877116ec347be8d089ae9c5a2dd46bd3
c1felode <abdusamadov.workit@gmail.com>
2026-08-10 19:52:22 +0500

    admin/payments completed shadcn ui components added

--- src/app/admin/bookings/components/total-bookings.tsx
+++ src/app/admin/bookings/components/total-bookings.tsx
@@ -4,21 +4,21 @@ import { AdvancedBookingsTable } from "./admin-bookings-table";
 export default function TotalBookings() {
     return (
         <div className="grid grid-cols-4 gap-6">
-            <div className="border-accent border rounded-md h-32 p-5 flex flex-col gap-4">
+            <div className="border-accent border rounded-md h-32 p-5 flex flex-col gap-4 bg-white">
                 <div className="flex justify-between">
                     <h3 className="text-sm text-gray-500 font-medium uppercase">Today's Bookings</h3>
                     <Button variant={"default"}><ClipboardClock /></Button>
                 </div>
                 <div className="flex gap-2 items-center"><span className="text-3xl font-semibold">1,284</span><span className="text-(--admin-green) flex items-center"><TrendingUp size={12} />12%</span></div>
             </div>
-            <div className="border-accent border rounded-md h-32 p-5 flex flex-col gap-4">
+            <div className="border-accent border rounded-md h-32 p-5 flex flex-col gap-4 bg-white">
                 <div className="flex justify-between">
                     <h3 className="text-sm text-gray-500 font-medium uppercase">Pending</h3>
                     <Button variant={"ghost"}><Clock /></Button>
                 </div>
                 <div className="flex gap-2 items-center"><span className="text-3xl font-semibold">48.2k</span><span className="text-(--admin-green) flex items-center"><TrendingUp size={12} />8.4%</span></div>
             </div>
-            <div className="border-accent border rounded-md h-32 p-5 flex flex-col gap-4">
+            <div className="border-accent border rounded-md h-32 p-5 flex flex-col gap-4 bg-white">
                 <div className="flex justify-between">
                     <h3 className="text-sm text-gray-500 font-medium uppercase">Completion Rate</h3>
                     <Button variant={"default"}><CircleCheckBig /></Button>
--- src/app/admin/payments/page.tsx
+++ src/app/admin/payments/page.tsx
@@ -1,3 +1,31 @@
+import TotalPayments from "./components/total-payments";
+import { AnalyticsDashboard } from "./components/analytics-dashboard";
+import { RecentTransactionsTable } from "./components/transactions-table";
+import {transactionsData} from './lib/transactions-data'
+
 export default function AdminPayments() {
-  return <div>Admin Payments</div>;
+  return <div>
+    <div className="flex items-end justify-between">
+      <div className="flex flex-col gap-4">
+        <div className="block"><span className="rounded-md bg-(--admin-gray) text-xs py-1 px-3">PAGE: ADMIN_PAYMENTS | DS: SALONFLOW_V1</span></div>
+        <h1 className="text-5xl font-bold">Payments & Revenue</h1>
+        <p className="text-sm text-muted-foreground">
+          Overview of financial performance and recent transactions.
+        </p>
+        {/* Add your salon management components here */}
+      </div>
+      <div className="space-x-2">
+      </div>
+    </div>
+
+    <div className="mt-12">
+      <TotalPayments/>
+    </div>
+    <div className="mt-16">
+      <AnalyticsDashboard/>
+    </div>
+    <div className="mt-12">
+      <RecentTransactionsTable data={transactionsData}/>
+    </div>
+  </div>;
 }
